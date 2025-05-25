a750db9de007831adeea26eeda99fd13a0be7a24
Utsav Poddar <utsavpoddar002@gmail.com>
2025-05-25 21:01:00 +0530

    Major changes

--- satellite/src/main/java/backend/satellite/SatelliteApplication.java
+++ satellite/src/main/java/backend/satellite/SatelliteApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SatelliteApplication {
 
     public static void main(String[] args) {
+        // System.out.println("DB_URL from env: " + System.getenv("DB_URL"));
+        // System.out.println("DB_USER from env: " + System.getenv("DB_USER"));
+        // System.out.println("DB_PASSWORD from env: " + System.getenv("DB_PASSWORD"));
         SpringApplication.run(SatelliteApplication.class, args);
         System.out.println("Satellite Application Started");
     }
